d91d4dfb39a10501cfca7ac74949e662193ea0e4
wuchangdong <1737149371@qq.com>
2019-06-27 19:28:07 +0800

    es6基础

--- day01/templstr.js
+++ day01/templstr.js
@@ -4,3 +4,13 @@ console.log(`名字：${name},年纪：${age}`);
 
 // 原理
 // 正则匹配替换
+
+
+let address1 = 'http://www.baidu.com';
+let address2 = 'ftp://www.baidu.com';
+// startsWith（''）判断字符串是否以这个开头
+// endsWidth 以什么结尾
+// includes 包含  就是 indexof !== -1
+// repeat(数字)
+let a = 'x';
+console.log(a.repeat(3)); //  xxx
