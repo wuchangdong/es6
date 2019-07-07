2b50e6978b519bf86c1ec17807461d776bd452f0
wuchangdong <1737149371@qq.com>
2019-07-07 22:58:15 +0800

    'promise'

---
+++ day05/promise.js
@@ -0,0 +1,24 @@
+/**
+ * @file promise
+ * @author: wuchangdong
+ */
+
+
+let p = new Promise(function (resolve, reject) {
+    let num = Math.random() * 10;
+    console.log(num);
+    setTimeout(function () {
+        if (num > 5) {
+            resolve('成功');
+        }
+        else {
+            reject('失败');
+        }
+    }, 2000);
+});
+
+p.then(res => {
+    console.log(res);
+}, rej => {
+    console.log(rej);
+});
