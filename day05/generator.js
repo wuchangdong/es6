a63b81d92b229dd25a3f554b2277ad1bb60012ce
wuchangdong <1737149371@qq.com>
2019-07-15 19:08:46 +0800

    gen

--- day05/generator.js
+++ day05/generator.js
@@ -3,3 +3,17 @@
  * @author  wuchangdong
  */
 
+function *read() {
+    let a = 'a';
+    console.log(a);
+    let b = yield a = a + 1;
+    console.log(a);
+    console.log(b);
+    let c = yield b + 2;
+    console.log(c);
+}
+
+let r = read();
+r.next();
+r.next(2);
+r.next(2);
